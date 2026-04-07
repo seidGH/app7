773fc4722a87499d546947d71ede985b801de3fd
seid <seyamen@gmail.com>
2026-04-07 12:56:03 +0300

    port mapping done by gocker 3000:5000

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 
